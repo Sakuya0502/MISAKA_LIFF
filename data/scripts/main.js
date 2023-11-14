ee7347011e96c8bdcc3cd9c4a76fee6afd634f18
Sakuya <52205413+Sakuya0502@users.noreply.github.com>
2023-11-14 14:56:13 +0800

    Fixed JSON issue

--- data/scripts/main.js
+++ data/scripts/main.js
@@ -458,7 +458,7 @@ function sendLiffMessage() {
         } else if (type == "messages") {
             console.log("Start sending message");
             var inputval = document.getElementById("messages").value;
-            var newval = inputval.replaceAll("\'", "\"");
+            var newval = inputval.replaceAll("\"", "\'");
             var messages = JSON.parse(newval);
             liff.sendMessages(messages).then(() => {
                 console.log("Success sending message");
